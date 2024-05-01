b7daa5080f633419a1078146d562e36eca1c2af1
kenjijiii <rzzlknjtrndd@gmail.com>
2024-05-01 23:49:06 +0800

    Handle Validate Order, and Initiate Allocate Order

--- src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
+++ src/main/java/guru/sfg/beer/inventory/service/config/JmsConfig.java
@@ -9,6 +9,8 @@ import org.springframework.jms.support.converter.MessageType;
 @Configuration
 public class JmsConfig {
     public static final String NEW_INVENTORY_REQUEST_QUEUE = "new-inventory-request";
+    public static final String QUEUE_VALIDATE_ORDER = "validate-order";
+
     @Bean
     public MappingJackson2MessageConverter jacksonMessageConverter(ObjectMapper objectMapper){
         MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
